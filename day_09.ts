da8f30aa24d55670cbaca1e721fe0f2c1c75a609
Luis Escobar Sawa <6599003+LeJawa@users.noreply.github.com>
2024-12-09 12:46:36 +0000

    Finish day 9 part 1 and start part 2

--- day_09.ts
+++ day_09.ts
@@ -8,29 +8,29 @@ if (actualFile.length % 2 === 0) {
 const calculatePart1Checksum = (file: string) => {
   let forwardIndex = 0;
   let backwardIndex = file.length - 1;
-  
+
   let spaceToAllocate = parseInt(file[backwardIndex]);
-  
+
   // let fileIndex = 0
-  
+
   let checksum = 0;
   let memoryIndex = 0;
-  
+
   const fileIndex = (index: number) => Math.trunc(index / 2);
-  
+
   while (forwardIndex < backwardIndex) {
     // Do file
     const fileSize = parseInt(file[forwardIndex]);
-  
+
     for (let i = 0; i < fileSize; i++, memoryIndex++) {
       checksum += memoryIndex * fileIndex(forwardIndex);
     }
-  
+
     forwardIndex++;
-  
+
     // Do empty space
     let emptySpace = parseInt(file[forwardIndex]);
-  
+
     while (emptySpace > 0) {
       if (spaceToAllocate > 0) {
         spaceToAllocate--;
@@ -38,15 +38,15 @@ const calculatePart1Checksum = (file: string) => {
         backwardIndex -= 2;
         spaceToAllocate = parseInt(file[backwardIndex]) - 1;
       }
-  
+
       checksum += memoryIndex * fileIndex(backwardIndex);
       memoryIndex++;
       emptySpace--;
     }
-  
+
     forwardIndex++;
   }
-  
+
   if (forwardIndex === backwardIndex) {
     while (spaceToAllocate > 0) {
       checksum += memoryIndex * fileIndex(backwardIndex);
@@ -55,16 +55,66 @@ const calculatePart1Checksum = (file: string) => {
     }
   }
 
-  return checksum
-}
+  return checksum;
+};
 
 const calculatePart2Checksum = (file: string) => {
-  
-}
+  const availableSpaces: number[][] = [];
+  const filesToMove: number[][] = [];
 
-const part1Checksum = calculatePart1Checksum(actualFile)
-const part2Checksum = calculatePart2Checksum(actualFile)
+  let runningIndex = 0;
+  Array.from(file).map((x) => parseInt(x)).forEach((size, i) => {
+    if (i % 2 === 0) {
+      filesToMove.push([i / 2, runningIndex, size]);
+    } else {
+      availableSpaces.push([runningIndex, size]);
+    }
+    runningIndex += size;
+  });
 
+  filesToMove.reverse();
+
+  let checksum = 0;
+
+  Array.from(filesToMove).forEach(
+    ([fileIndex, startFileIndex, size]) => {
+      if (startFileIndex < availableSpaces[0][0]) {
+        return;
+      }
+
+      if (
+        !availableSpaces.some(([startEmptyIndex, emptySize], i, arr) => {
+          if (size <= emptySize) {
+            let spaceToAllocate = size;
+            let memoryIndex = startEmptyIndex;
+            while (spaceToAllocate > 0) {
+              checksum += memoryIndex * fileIndex;
+              memoryIndex++;
+              spaceToAllocate--;
+            }
+            // Remove entry if emptySize - size == 0
+            arr[i] = [startEmptyIndex + size, emptySize - size];
+            return true;
+          }
+          return false;
+        })
+      ) {
+        let spaceToAllocate = size;
+        let memoryIndex = startFileIndex;
+        while (spaceToAllocate > 0) {
+          checksum += memoryIndex * fileIndex;
+          memoryIndex++;
+          spaceToAllocate--;
+        }
+      }
+    },
+  );
+
+  return checksum;
+};
+
+const part1Checksum = calculatePart1Checksum(actualFile);
+const part2Checksum = calculatePart2Checksum(actualFile);
 
 console.log(`Part 1: ${part1Checksum}`); // 6346871685398
-console.log(`Part 2: ${part2Checksum}`); // 
+console.log(`Part 2: ${part2Checksum}`); // 8546756871770 too high
