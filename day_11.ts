5a4b86dfef71602422799b8ce8f41d32fa30d160
Luis Escobar Sawa <6599003+LeJawa@users.noreply.github.com>
2024-12-11 10:50:28 +0000

    Failed optimization

--- day_11.ts
+++ day_11.ts
@@ -3,56 +3,57 @@ const file = Deno.readTextFileSync("input/day_11.txt");
 
 const startingStones: number[] = file.split(" ").map((x) => parseInt(x));
 
-const doBlink = (stones: number[]) => {
-  const newStones: number[] = [];
-  stones.forEach((stone, i, arr) => {
-    if (stone === 0) arr[i] = 1;
-    else {
-      const stringStone = stone.toString();
-      if (stringStone.length % 2 === 1) {
-        arr[i] *= 2024;
-      } else {
-        const half = stringStone.length / 2;
-        arr[i] = parseInt(stringStone.slice(0, half));
-        newStones.push(parseInt(stringStone.slice(half)));
-      }
-    }
-  });
-  stones = stones.concat(newStones);
-  return stones;
+const doBlink = (stone: number) => {
+  if (stone === 0) return [1];
+
+  const stringStone = stone.toString();
+  if (stringStone.length % 2 === 1) return [stone * 2024];
+
+  const half = stringStone.length / 2;
+  return [
+    parseInt(stringStone.slice(0, half)),
+    parseInt(stringStone.slice(half)),
+  ];
 };
 
-const doNBlinks = (stones: number[], n: number) => {
-  let blink = 0;
+const doNBlinks = (stone: number, n: number) => {
+  if (n === 0) return 1;
+  let stoneNumber = 0;
 
-  while (blink < n) {
-    stones = doBlink(stones);
-    blink++;
-    console.log(blink, stones.length);
-  }
+  const stones = doBlink(stone);
+  stones.forEach((newStone) => {
+    stoneNumber += doNBlinks(newStone, n - 1);
+  });
 
-  return stones;
+  return stoneNumber;
 };
 
 const calculatePart1 = (stones: number[]) => {
   const totalBlinks = 25;
-  return doNBlinks(stones, totalBlinks);
+  let stoneNumber = 0;
+  stones.forEach((stone) => {
+    stoneNumber += doNBlinks(stone, totalBlinks);
+  });
+  return stoneNumber;
 };
 
 const calculatePart2 = (stones: number[]) => {
   const totalBlinks = 75;
-  return doNBlinks(stones, totalBlinks);
+  let stoneNumber = 0;
+  stones.forEach((stone) => {
+    stoneNumber += doNBlinks(stone, totalBlinks);
+    console.log(`Stones counted ${stoneNumber}`);
+  });
+  return stoneNumber;
 };
 
 const t1 = performance.now();
-const part1Stones = calculatePart1(startingStones.slice());
-const part1 = part1Stones.length;
+const part1 = calculatePart1(startingStones.slice());
 const t2 = performance.now();
-const part2Stones = calculatePart2(startingStones.slice());
-const part2 = part2Stones.length;
+const part2 = calculatePart2(startingStones.slice());
 const t3 = performance.now();
 
-console.log(`Part 1: ${part1}`); //
+console.log(`Part 1: ${part1}`); // 231278
 console.log(`Part 2: ${part2}`); //
 
 console.log(`Start up took ${(t1 - t0).toFixed(3)} milliseconds.`);
