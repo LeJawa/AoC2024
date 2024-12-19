c40b1af394d0364bcd4c8e72a26f7fb0200e4cea
Luis Escobar Sawa <6599003+LeJawa@users.noreply.github.com>
2024-12-19 13:52:41 +0000

    Try day 19 part 2

--- day_19.ts
+++ day_19.ts
@@ -4,7 +4,7 @@ const file = Deno.readTextFileSync("input/day_19.txt");
 const towels = file.split("\n")[0].split(", ");
 const patterns = file.split("\n").slice(2);
 
-const possiblePatterns = (patterns: string[], towels: string[]) => {
+const possiblePatternsRegex = (patterns: string[], towels: string[]) => {
   const possible: string[] = [];
 
   const towelRegex = new RegExp(`^(?:${towels.join("|")})+$`);
@@ -16,14 +16,57 @@ const possiblePatterns = (patterns: string[], towels: string[]) => {
   return possible;
 };
 
+const towelDict: { [letter: string]: string[] } = {};
+
+towels.forEach((towel) => {
+  const letter = towel[0];
+
+  if (towelDict[letter] === undefined) towelDict[letter] = [];
+
+  towelDict[letter].push(towel);
+});
+
+const possiblePatternCombinations = (patterns: string[], towels: string[]) => {
+  let totalCombinations = 0;
+
+  patterns.forEach((pattern) => {
+    let combinations = 0;
+    const queue: string[] = [pattern];
+
+    while (queue.length > 0) {
+      const subpattern = queue.pop()!;
+
+      if (subpattern === "") {
+        combinations++;
+        continue;
+      }
+
+      towelDict[subpattern[0]].forEach((towel) => {
+        if (subpattern.startsWith(towel)) {
+          queue.push(subpattern.slice(towel.length));
+        }
+      });
+    }
+
+    console.log(`Pattern ${pattern}: ${combinations}`);
+    totalCombinations += combinations;
+  });
+
+  return totalCombinations;
+};
+
 const calculatePart1 = () => {
-  const possible = possiblePatterns(patterns, towels);
+  const possible = possiblePatternsRegex(patterns, towels);
 
   return possible.length;
 };
 
 const calculatePart2 = () => {
-  const possible = possiblePatterns(patterns, towels);
+  const possible = possiblePatternCombinations(
+    possiblePatternsRegex(patterns, towels),
+    towels,
+  );
+  return possible;
 };
 
 const t1 = performance.now();
